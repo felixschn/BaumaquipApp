bf7b66436eeecd17afa32686b0c7c13979024ec2
Felix <s6684276@msx.tu-dresden.de>
2020-12-09 10:09:28 +0100

    initial commit

---
+++ app/src/main/java/com/example/viewpager_test/MainPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.viewpager_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
