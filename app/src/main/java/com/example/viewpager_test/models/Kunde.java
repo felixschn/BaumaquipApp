31565c89978e7b3c53052aa2955ec1cfe946bea5
Felix <s6684276@msx.tu-dresden.de>
2020-12-30 12:16:11 +0100

    editing Kunden Class attributes

---
+++ app/src/main/java/com/example/viewpager_test/models/Kunde.java
@@ -0,0 +1,96 @@
+package com.example.viewpager_test.models;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Fts4;
+import androidx.room.PrimaryKey;
+
+@Fts4
+@Entity
+public class Kunde {
+    @PrimaryKey
+    public int rowid;
+
+    @NonNull
+    private String name;
+    @NonNull
+    private String telefonNumber;
+    @NonNull
+    private String streetName;
+    @NonNull
+    private String zip;
+    @NonNull
+    private String location;
+    @NonNull
+    private String constructionSide;
+    @NonNull
+    private String contactPerson;
+
+    public Kunde(int rowid, @NonNull String name, @NonNull String telefonNumber, @NonNull String streetName, @NonNull String zip, @NonNull String location, @NonNull String constructionSide, @NonNull String contactPerson) {
+
+        this.rowid = rowid;
+        this.name = name;
+        this.telefonNumber = telefonNumber;
+        this.streetName = streetName;
+        this.zip = zip;
+        this.location = location;
+        this.constructionSide = constructionSide;
+        this.contactPerson = contactPerson;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefonNumber() {
+        return telefonNumber;
+    }
+
+    public void setTelefonNumber(String telefonNumber) {
+        this.telefonNumber = telefonNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getConstructionSide() {
+        return constructionSide;
+    }
+
+    public void setConstructionSide(String constructionSide) {
+        this.constructionSide = constructionSide;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+}
