d6763ebbaacff73b6214e937a9d6056b396dd8ab
Felix <s6684276@msx.tu-dresden.de>
2021-02-27 16:53:40 +0100

    create new AddKundenViewModel

---
+++ app/src/main/java/com/example/rentalApplication/ui/Kunde/AddKundenViewModel.java
@@ -0,0 +1,19 @@
+package com.example.rentalApplication.ui.Kunde;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.rentalApplication.models.Kunde;
+import com.example.rentalApplication.persistence.KundenRepository;
+
+public class AddKundenViewModel extends AndroidViewModel {
+    private KundenRepository kundenRepository;
+
+    public AddKundenViewModel(@NonNull Application application) {
+        super(application);
+        kundenRepository = new KundenRepository(application);
+    }
+    public void insert(Kunde kunde){ kundenRepository.insert(kunde);}
+}
