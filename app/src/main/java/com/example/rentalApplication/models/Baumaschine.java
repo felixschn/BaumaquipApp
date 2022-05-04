edd00df35712bb08fdd6ab8572e5a47389554d84
nobodyKnowsMyTrouble <Peter.Rudolph@tu-dresden.de>
2022-05-04 13:27:37 +0200

    added example data

--- app/src/main/java/com/example/rentalApplication/models/Baumaschine.java
+++ app/src/main/java/com/example/rentalApplication/models/Baumaschine.java
@@ -151,7 +151,74 @@ public class Baumaschine {
         return new Baumaschine[]{
                 new Baumaschine("Bagger", 1,new BigDecimal(100), new BigDecimal(180), new BigDecimal(2000), 4000.0, "gut","voll"),
                 new Baumaschine("Kran", 1,new BigDecimal(200), new BigDecimal(280), new BigDecimal(4000), 1000.0, "schlecht","voll"),
-                new Baumaschine("Schrauben", 100,new BigDecimal(200), new BigDecimal(280), new BigDecimal(4000),null,null,null)
+                new Baumaschine("Schrauben", 100,                   new BigDecimal(200), new BigDecimal(280), new BigDecimal(4000),null,null,null),
+
+                new Baumaschine("Stampfer BT 60", 1,                new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Vibrationsplatte BPR 50/ 55 D", 1, new BigDecimal(42),  new BigDecimal(63), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Rüttelplatte BVP 18/45", 1,        new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Flaschenrüttler", 1,               new BigDecimal(20),  new BigDecimal(30), new BigDecimal(9999), null, null, null),
+
+                // TODO: Katalog Baustelle
+                new Baumaschine("Stützen bis 2,50m A", 1000,       new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                new Baumaschine("Stützen bis 3,00m A", 1000,       new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                new Baumaschine("Stützen bis 3,00m B", 1000,       new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                new Baumaschine("Stützen bis 3,50m B", 1000,       new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                new Baumaschine("Stützen bis 4,00m B", 1000,       new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                new Baumaschine("Bauzaun Feld", 1000,              new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal("3.5"), null, null, null),
+                new Baumaschine("Bauzaun Fuß", 1000,               new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(2), null, null, null),
+                new Baumaschine("Bautüren", 1000,                  new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+                // TODO: Katalog Gerüste
+                new Baumaschine("Gerüste", 1,                  new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+
+                new Baumaschine("Kombihammer", 20,                new BigDecimal(20),  new BigDecimal(30), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Abbruchhammer 30 kg", 10,        new BigDecimal(38),  new BigDecimal(57), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Erdlochbohrer", 10,              new BigDecimal(25),  new BigDecimal(33), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Kernlochbohrer", 10,             new BigDecimal(38),  new BigDecimal(57), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Betonsilo", 1,                new BigDecimal(20),  new BigDecimal(30), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Bordsteinzange", 1,           new BigDecimal(10),  new BigDecimal(15), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Knacke", 1,                   new BigDecimal(22),  new BigDecimal(33), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Stapelpaletten", 1,           new BigDecimal(9999),  new BigDecimal(9999), new BigDecimal(5), null, null, null),
+
+                new Baumaschine("Verputzmaschine VR1", 1, new BigDecimal(95),  new BigDecimal("142.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Verputzmaschine VR6", 1, new BigDecimal(100),  new BigDecimal(150), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Durchlaufmischer VR2302", 1, new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Stielreibe", 1, new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Rührer", 1, new BigDecimal(17),  new BigDecimal("25.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Airless Farbspritzgerät", 1, new BigDecimal(45),  new BigDecimal("67.5"), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Rotationslaser", 1, new BigDecimal(30),  new BigDecimal(45), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Bautrockner 158 l/Tag", 1, new BigDecimal(20),  new BigDecimal(30), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Bautrockner  43 l/Tag", 1, new BigDecimal(20),  new BigDecimal(30), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Heizgeräte 3,0 kW", 1, new BigDecimal(8),  new BigDecimal(12), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Heizgeräte 9,0 kW", 1, new BigDecimal(10),  new BigDecimal(15), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Laubbläser", 1,        new BigDecimal(14),  new BigDecimal(26), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Schneefräse", 1,       new BigDecimal(55),  new BigDecimal("82.5"), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Fugenschneider", 1,  new BigDecimal(33),  new BigDecimal("49.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Tischssäge", 1,      new BigDecimal(38),  new BigDecimal(57), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Baukreissäge", 1,    new BigDecimal(35),  new BigDecimal(52), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Trennschneider", 1,  new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Bandsäge", 1,        new BigDecimal(35),  new BigDecimal(44), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Betonschleifer", 1,  new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Spezialsäge", 1,     new BigDecimal(35),  new BigDecimal("52.5"), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Stromerzeuger", 1,  new BigDecimal(35),  new BigDecimal("52.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Kompressor", 1,     new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Hochdruckreiniger", 1,     new BigDecimal(28),  new BigDecimal(42), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Hochdruckreiniger Heißwasser", 1,  new BigDecimal(38),  new BigDecimal(57), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Nass-/ Trockensauger", 1,  new BigDecimal(22),  new BigDecimal(33), new BigDecimal(9999), null, null, null),
+                new Baumaschine("C-Pumpe", 1,               new BigDecimal(25),  new BigDecimal("37.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("B-Pumpe", 1,               new BigDecimal(35),  new BigDecimal("52.5"), new BigDecimal(9999), null, null, null),
+
+                new Baumaschine("Bagger 300.9 D", 1,                new BigDecimal(75),  new BigDecimal("112.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Kurzheckbagger 301.6-05 A", 1,     new BigDecimal(83),  new BigDecimal("124.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Kurzheckbagger 302.7D CR", 1,      new BigDecimal(91),  new BigDecimal("136.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Kurzheckbagger 303.5 E2 CR", 1,    new BigDecimal(99),  new BigDecimal("148.5"), new BigDecimal(9999), null, null, null),
+                new Baumaschine("Mini Dumper", 1,                   new BigDecimal(30),  new BigDecimal(45), new BigDecimal(9999), null, null, null)
+
         };
     }
 }
